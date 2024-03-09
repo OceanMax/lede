ba6a5fb94fdff3f5413075d8bd2b6f6e38239c5c
OceanMax <96224948+OceanMax@users.noreply.github.com>
2024-03-09 14:53:07 +0800

    Delete

--- configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Modify default system settings
-
-# passwall
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
