b8a6b6c51de4dc1ab03acc587f839ec0a005479b
OceanMax <96224948+OceanMax@users.noreply.github.com>
2024-03-13 15:28:13 +0800

    Update

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,15 +9,15 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script (After Update feeds)
 
-# helloworld
-echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
+# passwall
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 
 # openclash
 echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
 
-# passwall
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+# helloworld
+echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
 
 # netdata
 echo "src-git netdata https://github.com/Jason6111/luci-app-netdata.git;main" >> feeds.conf.default
